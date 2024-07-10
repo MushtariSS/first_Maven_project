48bd84a07c17e60df02f378b3eb3b4f992e6ea94
XPL <mushti.romin@gmail.com>
2024-07-09 21:20:37 -0400

    my  commitment

---
+++ src/test/java/practice.java
@@ -0,0 +1,8 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class practice {
+    public static void main(String[] args) {
+        ChromeDriver chromeDriver = new ChromeDriver();
+        chromeDriver.get("https://facebook.com");
+    }
+}
