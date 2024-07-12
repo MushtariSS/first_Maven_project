cc3d0bfce566d3aea1d229742859b25ab016a76f
XPL <mushti.romin@gmail.com>
2024-07-11 20:50:23 -0400

    my  commitment

---
+++ src/test/java/review_week_1/activity_1.java
@@ -0,0 +1,45 @@
+package review_week_1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class activity_1 {
+    public static void main(String[] args) throws InterruptedException {
+        EdgeDriver driver = new EdgeDriver();
+        Thread.sleep(3000);
+        driver.manage().window().maximize();
+        driver.get("https://retail.tekschool-students.com");
+
+        By signInLocator = By.id("sign in");
+        WebElement signInElement=driver.findElement(signInLocator);
+        signInElement.click();
+
+        By createNewAccount = By.id("create new account");
+        WebElement createNewAccountElement=driver.findElement(createNewAccount);
+        createNewAccountElement.click();
+
+        By name = By.id("nameInput");
+        WebElement nameElement = driver.findElement(name);
+        nameElement.sendKeys("Mushtari");
+
+        By email = By.id("email input");
+        WebElement emailElm = driver.findElement(email);
+        emailElm.sendKeys("m.saidjafaropva@gmail.com");
+
+        By password = By.id("passwordInput");
+        WebElement passwordElm = driver.findElement(password);
+        emailElm.sendKeys("Romin2021!");
+
+        By confirmPassword = By.id("confirmPasswordInput");
+        WebElement confirmPasswordElm = driver.findElement(confirmPassword);
+        confirmPasswordElm.sendKeys("Romin2021!");
+        
+        By signupBtn= By.id("signupBtn");
+        WebElement signupBtnElm = driver.findElement(signupBtn);
+        signupBtnElm.click();
+
+
+    }
+}
